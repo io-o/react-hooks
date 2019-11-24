4ccd26fd89c7498a19583c9dfd8bbe246eecdcd1
kevinl7 <mygao666@gmail.com>
2019-11-24 22:07:18 +0800

    :tada: 初始化文件结构

---
+++ src/query/App.jsx
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux'
+
+function App(props) {
+  
+}
+
+export default connect(
+  function mapStateToProps(state) {},
+  function mapDispatchToProps(dispatch) {}
+)(App)
