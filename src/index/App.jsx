74b6fda810bf8ce8919e5034115e2041c6763a87
kevinl7 <mygao666@gmail.com>
2019-11-25 22:48:54 +0800

    :tada: 新增首页模块

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,10 +1,21 @@
+import React from 'react'
 import { connect } from 'react-redux'
 
+import Header from '../common/Header.jsx'
+
 function App(props) {
-  
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
 }
 
 export default connect(
-  function mapStateToProps(state) {},
-  function mapDispatchToProps(dispatch) {}
+  function mapStateToProps(state) {
+    return {}
+  },
+  function mapDispatchToProps(dispatch) {
+    return {}
+  }
 )(App)
